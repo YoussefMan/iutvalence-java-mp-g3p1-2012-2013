21def75c8e226362266297a6b0eba11c2a3f8b76
sebastienjean <baz.jean@gmail.com>
2012-11-11 17:26:45 +0100

    Renommage de la classe Etatdela partie en Partie Renommage du test Test en TestZone Creation d'une application TestPartie

--- src/fr/iutvalence/java/projets/Pacman/Partie.java
+++ src/fr/iutvalence/java/projets/Pacman/Partie.java
@@ -6,45 +6,28 @@ package fr.iutvalence.java.projets.Pacman;
  * @author elmanssy
  * 
  */
-// FIXME renommer la classe
-public class Etatdelapartie {
+// FIXME ne pas mettre de main dans cette classe, mais une méthode "demarrer" qui execute une partie
+// FIXME pour l'instant, implémenter un comportement simple où pacman se déplace seul avec une décision de déplacement
+// aléatoire (à discuter)
 
-	
+public class Partie
+{
+
 	private int nombredebillesinit;
 	private int nombredebilles;
 	private int nombredegommesinit;
 	private int nombredegommes;
 	private int score;
 
-	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 
-		ZoneDeJeux ZoneTest = new  ZoneDeJeux();
-		this.nombredebilles = ZoneTest.getBilles();
-		this.score = 0;
-		
-		
+		ZoneDeJeux ZoneTest = new ZoneDeJeux();
+		// this.nombredebilles = ZoneTest.getBilles();
+		// this.score = 0;
+
 		Pacman Perso = new Pacman();
-		Fantome F1 = new Fantome();
-		Fantome F2 = new Fantome();
-		
+
 	}
 
-    
-
-	
-	
-
-	
-	
-	
-
-
-	
-	
-
-	
-	}
-
-	
 }
--- src/fr/iutvalence/java/projets/Pacman/test/TestZone.java
+++ src/fr/iutvalence/java/projets/Pacman/test/TestZone.java
@@ -1,29 +1,32 @@
-
 package fr.iutvalence.java.projets.Pacman.test;
 
 import fr.iutvalence.java.projets.Pacman.ZoneDeJeux;
 
-// FIXME(FIXED) compléter le commentaire
 /**
- * Cette classe de test permet de tester nos classes, ici on affiche pour commencer la zone de jeu.
- * Dans un main on affiche chacune de nos classes.
+ * Cette classe de test permet de tester nos classes, ici on affiche pour commencer la zone de jeu. Dans un main on
+ * affiche chacune de nos classes.
+ * 
  * @author elmanssy
  * 
- *
- *
+ * 
+ * 
  */
 
-public class Test {
+public class TestZone
+{
 
 	/**
 	 * ce main permet de tester les differentes instances de classe
-	 * @param args (aucun)
+	 * 
+	 * @param args
+	 *            (aucun)
 	 * 
 	 * 
 	 */
-	public static void main(String[] args) {
-		
-		ZoneDeJeux ZoneTest = new  ZoneDeJeux();
+	public static void main(String[] args)
+	{
+
+		ZoneDeJeux ZoneTest = new ZoneDeJeux();
 		System.out.println(ZoneTest.toString());
 	}
 
